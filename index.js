383e21f7e2547f4fc7fa837812e1276522d0223d
Laura Travin <laura.travin@gmail.com>
2017-11-05 03:08:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-const app = "I don't do much."
+const app = "I don't do much.";
+
+
+var kittens = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendKitten(value){
+  kittens.push(value);
+  return kittens
+}
+
+var kittens = ["Milo", "Otis", "Garfield"];
+function destructivelyPrependKitten(value){
+  kittens.unshift(value);
+  return kittens
+}
+
+var kittens = ["Milo", "Otis", "Garfield"];
+function destructivelyRemoveLastKitten(value){
+  kittens.pop(value);
+  return kittens
+}
+
+var kittens = ["Milo", "Otis", "Garfield"];
+function destructivelyRemoveFirstKitten(value){
+  kittens.shift(value);
+  return kittens
+}
